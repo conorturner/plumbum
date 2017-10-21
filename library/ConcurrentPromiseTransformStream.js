319001e27cd3b6ba191235a56e753ee2288cfe16
Conor Turner <conorturner@live.com>
2017-10-21 22:15:01 +0100

    boom

--- library/ConcurrentPromiseTransformStream.js
+++ library/ConcurrentPromiseTransformStream.js
@@ -7,40 +7,39 @@ class ConcurrentPromiseTransformStream extends Transform {
 	constructor(concurrency, {transform}) {
 		super();
 		this.transform = transform;
-		this.slots = new Array(concurrency);
+		this.slots = new Array(concurrency).fill(null);
 		this.candidate = null;
 	}
 
 	_transform(chunk, enc, next) {
+		// console.log(chunk.toString())
 		this.takeSlot(chunk, next);
 	}
 
-	freeSlot(index, next) {
-		delete this.slots[index];
+	freeSlot(index) {
+		this.slots[index] = null;
 
 		if(this.candidate){
-			this.takeSlot(this.candidate);
+			const {chunk, next} = this.candidate;
 			this.candidate = null;
-			next();
-		}
-		else{
-			next();
+			this.takeSlot(chunk, next);
 		}
 	}
 
 	takeSlot (chunk, next) {
 		const freeSlots = this.slots
 			.map((slot, index) => ({slot, index}))
-			.filter(({slot}) => slot === undefined);
+			.filter(({slot}) => slot === null);
 
 		if(freeSlots.length === 0) {
-			this.candidate = chunk;
+			// console.log(this.candidate);
+			// console.log(freeSlots.length);
+
+			this.candidate = {chunk, next};
 		}
 		else {
 			const [{index}] = freeSlots;
-
-			this.slots[index] = this.process(chunk).then(() => this.freeSlot(index, next));
-
+			this.slots[index] = this.process(chunk).then(() => this.freeSlot(index));
 			next();
 		}
 	}
---
+++ test/test-concurrent-promise-transform-stream.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const {should} = require("chai");
+should();
+
+const ConcurrentPromiseTransformStream = require("../library/ConcurrentPromiseTransformStream");
+
+describe("Test", () => {
+
+	it("Basic Test", (done) => {
+
+		const { Readable } = require('stream');
+
+		class MockReadable extends Readable {
+			_read(size) {
+				this.counter = this.counter || 0;
+				this.counter++;
+				// console.log("being read")
+				if(this.counter < 10 + 1) this.push("500");
+			}
+		}
+
+		const mockReadable = new MockReadable();
+
+		const transform = (item) => new Promise((resolve) => setTimeout(() => resolve(item), item));
+		const pStream = new ConcurrentPromiseTransformStream(3, {transform});
+
+
+		pStream.on("data", (chunk) => console.log(chunk.toString()));
+		pStream.on("end", (err) => done(err));
+
+		mockReadable.pipe(pStream);
+
+		setTimeout(done, 5000);
+
+	}).timeout(10000);
+
+});
